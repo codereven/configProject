9618942552c866dd391937dcea90d08887586e7f
changzhenxing <changzx@cnnho.com>
2020-04-23 17:03:52 +0800

    配置集群继承eureka

--- config-server/src/main/java/com/example/configserver/ConfigServerApplication.java
+++ config-server/src/main/java/com/example/configserver/ConfigServerApplication.java
@@ -3,10 +3,12 @@ package com.example.configserver;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.config.server.EnableConfigServer;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 @SpringBootApplication
 //开始远程配置服务注解
 @EnableConfigServer
+@EnableEurekaClient
 public class ConfigServerApplication {
 
     public static void main(String[] args) {
